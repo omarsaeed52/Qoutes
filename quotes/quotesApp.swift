3b3ac40b382505ef04ba30ef39cc420c704b663c
omar <omar.123.ahmed@hotmail.com>
2021-04-26 11:03:15 +0200

    Initial Commit

---
+++ quotes/quotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  quotesApp.swift
+//  quotes
+//
+//  Created by omar ahmed on 26/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct quotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
